872b7e107c7038541409ca60bfa26118de74932e
Nizeh <Benjamssepulveda@gmail.com>
2023-11-03 02:02:14 -0300

    codigo comentado

--- Assets/Scripts/VidaApple.cs
+++ Assets/Scripts/VidaApple.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class VidaApple : MonoBehaviour
 {
 
-    public static void ItemManzana(Collision2D collision,int Life)
+    public static void ItemManzana(Collision2D collision,int Life) //Cada vez que el jugador colisione
     {
-        if (Life < 5 )
+        if (Life < 5 ) //Si Life es menor que 5 entonces se ejecuta:
         {
-            Life += 3;
-            Destroy( collision.gameObject );
+            Life += 3; //Se le suma 3 puntos de vida con la Manzana al jugador
+            Destroy( collision.gameObject ); //Se destruye la manzana una vez el jugador entra en contacto
         
         }
-        else if  (Life >= 5 ) 
+        else if  (Life >= 5 ) // Si Life es mayor o igual a 5 se ejecura: 
         {
-            Life = 5;
-            Destroy( collision.gameObject );
+            Life = 5; //Se queda igual para no curar
+            Destroy( collision.gameObject ); // Se destruye
         }
     }
     public static void ItemPlatano(Collision2D collision, int Life)
